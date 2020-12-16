7449a2e030cae1deb81f3a073c3515bf021455a9
unknown <james-scofield>
2020-12-16 19:53:46 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+ 
+app.use(express.json());
+
+// production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
